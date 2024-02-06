f331a5fcc34b7a025ab5ce53ea45fcf9b08c77aa
Nu Pogodyy <Odontuyadorj2@gmail.com>
2024-02-06 13:00:55 +0800

    star rating & review
    css bhgu bga busdaar duussan

--- html/fetch_reviews.php
+++ html/fetch_reviews.php
@@ -1,30 +1,27 @@
 <?php
-// fetch_reviews.php
-
 $host = "localhost";
 $user = "postgres";
 $pass = "9021";
 $db = "form";
 
-// Connect to PostgreSQL server
 $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die("Unable to connect to PostgreSQL server");
 
 if (!$con) {
     echo "Error: Unable to connect to database.";
 } else {
     if (isset($_GET['sort']) && $_GET['sort'] === 'sort_by_stars') {
-        // Fetch reviews from the database sorted by star rating (descending order)
+        // star buurah daraallaar
         $query = "SELECT * FROM review ORDER BY star_number DESC";
     } else {
-        // Fetch reviews from the database (without sorting)
+        // sortguigeer haruulah
         $query = "SELECT * FROM review";
     }
-    // Fetch reviews from the database
+    // fetch database display hiih
     $query = "SELECT * FROM review";
     $result = pg_query($con, $query);
 
     if ($result) {
-        // Calculate overall star rating and total number of comments
+        // buh comment, dundaj star rate hiih
         $totalStars = 0;
         $totalComments = pg_num_rows($result);
 
@@ -34,34 +31,34 @@ if (!$con) {
 
         $overallStarRating = ($totalComments > 0) ? round($totalStars / $totalComments, 1) : 0;
 
-        // Output overview section
+     
         echo "<div class='overview'>";
         echo "<h2>Overview</h2>";
         echo "<p><strong>Overall Star Rating:</strong> $overallStarRating</p>";
         echo "<p><strong>Total Comments:</strong> $totalComments</p>";
         echo "</div>";
 
-        // Reset the pointer of the result set to the beginning
+        // ur dungiin eheleliig reset hiih
         pg_result_seek($result, 0);
         
 
-        // Loop through each review
+        // review neg bur dr loop
         while ($row = pg_fetch_assoc($result)) {
-            // Generate HTML block for each review
+  
             $html = "<div class='review'>";
             $html .= "<div><strong>Username:</strong> " . $row['username'] . "</div>";
             $html .= "<div><strong>Stars:</strong> ";
             
-            // Add star icons based on star_number with yellow color
+            // star shar ungutei gargaj ireh
             for ($i = 0; $i < $row['star_number']; $i++) {
-                $html .= "<span style='color: yellow;'>&#9733;</span>"; // Star symbol HTML entity with yellow color
+                $html .= "<span style='color: yellow;'>&#9733;</span>"; 
             }
             
             $html .= "</div>";
             $html .= "<div><strong>Comment:</strong> " . $row['comment'] . "</div>";
             $html .= "</div>";
 
-            // Output HTML for this review
+            // html ee output hiih
             echo $html;
         }
         
