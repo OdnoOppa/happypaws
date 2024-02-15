e13ebb7290aeb108e104fe9d6a349c83a6d12f17
temukk <119791505+temukk@users.noreply.github.com>
2024-02-15 21:38:56 +0800

    Update get_data.php

--- html/get_data.php
+++ html/get_data.php
@@ -6,15 +6,15 @@ $db = "form";
 
 $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die("Unable to connect to PostgreSQL server");
 
-$query = "SELECT * FROM form";
-$result = pg_query($con, $query);
+$query = "SELECT * FROM form"; // form хүснэгтээс бүх баганыг select хийж буй үйлдлийг $query хувьсагчид оноосон
+$result = pg_query($con, $query); // $result хувьсагчид дээрх query үйлдлийнн үр дүнг оноосон
 
-$data = array();
+$data = array(); // $data хоосон хүснэгтэн хувьсагч үүсгэсэн
 while ($row = pg_fetch_assoc($result)) {
-    $data[] = $row;
+    $data[] = $row; // $data хүснэгтэд $result хувьсагчийн утгыг мөр мөрөөр нь оноосон 
 }
 
-echo json_encode($data);
+echo json_encode($data); // $data хувьсагч дахь өгөгдлийг json_encode функц ашиглан json формат руу хөрвүүлсэн, дараагаар нь output хийсэн.
 
 pg_close($con);
 ?>
