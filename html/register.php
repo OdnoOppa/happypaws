0961438ece506b789afb56a5eab4a4bf75714949
temukk <119791505+temukk@users.noreply.github.com>
2024-02-15 21:37:58 +0800

    Update register.php

--- html/register.php
+++ html/register.php
@@ -2,9 +2,9 @@
 $db = pg_connect("host=localhost port=5432 dbname=form user=postgres password=9021");
 
 $username = $_POST['username'];
-$password = $_POST['password'];
+$password = $_POST['password']; // HTML-с авсан username password-н утгыг $username $password утгуудад онооно.
 
-$query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+$query = "INSERT INTO users (username, password) VALUES ('$username', '$password')"; // Шинээр бүртгүүлж байгаа учраас insert үйлдэд хийнэ.
 $result = pg_query($db, $query);
 
 if ($result) {
@@ -15,8 +15,10 @@ if ($result) {
     echo "<button style='font-family: \"Nunito\", sans-serif; padding: 10px 20px; background-color: #F1C439; color: white; border: none; border-radius: 8px; cursor: pointer;' onclick=\"window.location.href='login.html';\">Нэвтрэх</button>";
     echo "</div>";
     echo "</div>";
+    // Бүртгэл амжилттай бол амжилттай мессеж харуулна
 } else {
     echo "Error: " . pg_last_error($db);
+    // Бүртгэл амжилтгүй бол амжилтгүй мессеж харуулна.
 }
 
 pg_close($db);
