7bd19049d0367412230ce3d0ee74d25725965736
temukk <119791505+temukk@users.noreply.github.com>
2024-02-15 21:38:16 +0800

    Update login.php

--- html/login.php
+++ html/login.php
@@ -1,12 +1,12 @@
 <?php
 $db = pg_connect("host=localhost port=5432 dbname=form user=postgres password=9021");
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST['username']; 
+$password = $_POST['password']; // username болон password хувьсагчид утгуудыг онооно.
 
-$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result = pg_query($db, $query);
-$row = pg_fetch_assoc($result);
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'"; // username болон password таарч буй эсэхийг шалгана
+$result = pg_query($db, $query); // Result-д query-ний үр дүнг өгсөн
+$row = pg_fetch_assoc($result); // Амжилттай болвол $row -д $result оноосон
 
 if ($row) {
 
@@ -16,6 +16,8 @@ if ($row) {
     echo "<button style='font-family: \"Nunito\", sans-serif; padding: 10px 20px; background-color: #F1C439; color: white; border: none; border-radius: 8px; cursor: pointer;' onclick=\"window.location.href='ad-zar.html';\">Зар оруулах</button>";
     echo "</div>";
     echo "</div>";
+
+    // Нэвтрэлт амжилттай бол амжилттай гэсэн мессеж харуулна.
 } else {
     echo "Амжилтгүй. Дахиад оролдно уу.";
 }
